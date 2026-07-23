3041c7a81e2a7cdaf9fe37a60bbd7fb4fe836c9b
Adish Pandey <26panad@goodnews.vic.edu.au>
2026-07-23 12:03:51 +1000

    Current validation system

--- Kajana/app.py
+++ Kajana/app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("Login.html")
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+
+    error = None
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+
+        # Example valid username
+        if username != "admin":
+            error = "Invalid username. Please try again."
+
+    return render_template("Login.html", error=error)
 
 
 if __name__ == "__main__":
