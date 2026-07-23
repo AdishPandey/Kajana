0ab964102e591dee6ed1c7236f6ae0e3aad94cb2
Adish Pandey <26panad@goodnews.vic.edu.au>
2026-07-23 12:48:01 +1000

    Created better validation/ Login leading to Dashboard

--- Kajana/app.py
+++ Kajana/app.py
@@ -10,14 +10,35 @@ def login():
 
     if request.method == "POST":
 
-        username = request.form["username"]
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-        # Example valid username
-        if username != "admin":
-            error = "Invalid username. Please try again."
+
+        # Hard-coded login details
+        correct_username = "admin"
+        correct_password = "Password123!"
+
+
+        if username == correct_username and password == correct_password:
+
+            return render_template("dashboard.html")
+
+
+        else:
+            error = "Invalid username or password. Please try again."
+
 
     return render_template("Login.html", error=error)
 
 
+
+@app.route("/dashboard")
+def dashboard():
+
+    return render_template("dashboard.html")
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+    
